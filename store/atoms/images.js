1e336e300d1880ddaa008f58e9221e599ec03e7a
jonthefox <weissjon@gmail.com>
2022-03-08 09:34:35 +0200

    add images state

--- store/atoms/images.js
+++ store/atoms/images.js
@@ -2,5 +2,5 @@ import { atom } from "recoil";
 
 export default atom({
   key: "images", // unique ID (with respect to other atoms/selectors)
-  default: [], // initial value
+  default: [], // initial valuea
 });
